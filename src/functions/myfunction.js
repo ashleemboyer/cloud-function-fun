7df598f48208a8a632893ef6b09b2ae298f7b6a6
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-08-12 12:34:16 -0400

    Still testing putting html strings in query params

--- src/functions/myfunction.js
+++ src/functions/myfunction.js
@@ -1,15 +1,16 @@
 const htmlToImage = require('html-to-image')
 
 exports.handler = function(event, context, callback) {
-  const json = JSON.stringify(event.queryStringParameters)
-  console.log(json.container, !!json.container)
+  const params = event.queryStringParameters
 
-  if (!!json.container) {
-    callback(null, {
-      body: {
-        container: decodeURIComponent(json.container),
-      },
-      statusCode: 200,
+  if (params.container) {
+    htmlToImage.toPng(params.container).then(dataUrl => {
+      callback(null, {
+        body: {
+          url: dataUrl,
+        },
+        statusCode: 200,
+      })
     })
   } else {
     callback(null, { body: 'Hmmmmmmm', statusCode: 200 })
