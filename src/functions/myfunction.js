f240c7fe45a298e332894e0e534263e342159728
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-08-12 14:37:54 -0400

    I need to find a better way to test this locally

--- src/functions/myfunction.js
+++ src/functions/myfunction.js
@@ -1,24 +1,23 @@
 var request = require('request')
 
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    body: JSON.stringify({ foo: 'bar' }),
-    statusCode: 200,
-  })
-  // const data = {
-  //   // css:
-  //   //   ".box { border: 4px solid #03B875; padding: 20px; font-family: 'Roboto'; }",
-  //   google_fonts: 'Roboto',
-  //   html: '<p>Hello, world!</p>',
-  // }
+  const data = {
+    // css:
+    //   ".box { border: 4px solid #03B875; padding: 20px; font-family: 'Roboto'; }",
+    // google_fonts: 'Roboto',
+    html: '<p>Hello, world!</p>',
+  }
 
-  // request
-  //   .post({ form: data, url: 'https://hcti.io/v1/image' })
-  //   .auth(
-  //     'e57af5e7-dbca-444f-bb62-a9fe06d4805f',
-  //     'f8387fc1-7a56-4c5a-ab11-015ea1a902f6'
-  //   )
-  //   .on('data', function(data) {
-
-  //   })
+  request
+    .post({ form: data, url: 'https://hcti.io/v1/image' })
+    .auth(
+      'e57af5e7-dbca-444f-bb62-a9fe06d4805f',
+      'f8387fc1-7a56-4c5a-ab11-015ea1a902f6'
+    )
+    .on('data', function(data) {
+      callback(null, {
+        body: JSON.stringify(data),
+        statusCode: 200,
+      })
+    })
 }
